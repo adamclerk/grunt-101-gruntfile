4b18d285a792fd45beafbaae235f357bbc96f9ed
Adam Clark <adamclerk@gmail.com>
2014-03-19 21:05:17 -0600

    initial commit

---
+++ template.js
@@ -0,0 +1,53 @@
+/*
+ * grunt-init-gruntfile
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a basic Gruntfile.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = 'This template tries to guess file and directory paths, but ' +
+  'you will most likely need to edit the generated Gruntfile.js file before ' +
+  'running grunt. _If you run grunt after generating the Gruntfile, and ' +
+  'it exits with errors, edit the file!_';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = 'Gruntfile.js';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({}, [
+    {
+      name: 'package_json',
+      message: 'Will you have a package.json file?',
+      default: 'Y/n',
+      warning: 'This changes how filenames are determined and banners are generated.'
+    }
+  ], function(err, props) {
+    props.package_json = /y/i.test(props.package_json);
+
+    // If is package_json true, generate package.json
+    if (props.package_json) {
+      var devDependencies = {
+        'grunt': '~0.4.2'
+      };
+
+      // Generate package.json file, used by npm and grunt.
+      init.writePackageJSON('package.json', {
+        node_version: '>= 0.10.0',
+        devDependencies: devDependencies
+      });
+    }
+
+    // All done!
+    done();
+  });
+
+};
