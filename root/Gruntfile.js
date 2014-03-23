d5e4e22d0b5ec12e0e29bc255ed7dc590f72c06a
Adam Clark <adamclerk@gmail.com>
2014-03-22 21:34:49 -0600

    updating gruntfile to pass new jshint

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -4,29 +4,29 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' + 
-      '<%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    pkg: grunt.file.readJSON("package.json"),
+    banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
+      "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n" +
+      "<%= pkg.homepage ? \"* \" + pkg.homepage + \"\\n\" : \"\" %>" +
+      "* Copyright (c) <%= grunt.template.today(\"yyyy\") %> " + 
+      "<%= pkg.author.name %>;" +
+      " Licensed <%= _.pluck(pkg.licenses, \"type\").join(\", \") %> */\n",
     meta: {
-      version: '0.1.0'
+      version: "0.1.0"
     },
     cat: {
       logo: {
-        file: 'title.txt'
+        file: "title.txt"
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-cat');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-cat");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
   // Default task.
-  grunt.registerTask('default', ['cat:logo']);
+  grunt.registerTask("default", ["cat:logo"]);
 
 };
